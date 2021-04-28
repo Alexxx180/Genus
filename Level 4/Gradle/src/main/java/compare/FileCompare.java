f8e0a6931d16ce6e8c4f5787df474b4cba7df895
Alexxx180 <alexx120702@gmail.com>
2021-04-28 11:30:59 +0300

    Level 4.1

--- Level 4/Gradle/src/main/java/compare/FileCompare.java
+++ Level 4/Gradle/src/main/java/compare/FileCompare.java
@@ -1,34 +1,72 @@
 package compare;
 
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.Appender;
+import org.apache.logging.log4j.core.Layout;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.appender.FileAppender;
+import org.apache.logging.log4j.core.config.Configuration;
+import org.apache.logging.log4j.core.config.LoggerConfig;
+import org.apache.logging.log4j.core.layout.PatternLayout;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class FileCompare {
-    public String[] compare(String fName1, String fName2) throws IOException {
+
+    static Logger log = LogManager.getLogger("FileCompare");
+
+    public ArrayList<String> compare(String fName1, String fName2) throws IOException {
+
+        final String LOG_PATTERN = "%d{yyyy-MM-dd HH:mm:ss} [%-5level] MyApp - %msg%n";
+        final LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
+        final Configuration config = ctx.getConfiguration();
+
+        Layout layout = PatternLayout.createLayout(LOG_PATTERN, null, config, null, null, false, false, null, null);
+        Appender appenderInfo = FileAppender.createAppender("Logs/Info.log", "true", "true", "File", "true",
+                "false", "false", null, layout, null, "false", null, config);
+        Appender appenderDebug = FileAppender.createAppender("Logs/Debug.log", "true", "true", "File2", "true",
+                "false", "false", null, layout, null, "false", null, config);
+        appenderInfo.start();
+        appenderDebug.start();
+        config.addAppender(appenderInfo);
+        config.addAppender(appenderDebug);
+        LoggerConfig loggerConfig = config.getLoggerConfig("compare.FileCompare");
+        loggerConfig.addAppender(appenderInfo, Level.INFO, null);
+        loggerConfig.addAppender(appenderDebug, Level.DEBUG, null);
+        ctx.updateLoggers();
+
         String line1, line2;
-        String[] files = { null, null };
+        ArrayList<String> files = new ArrayList<String>();
         int i=1;
+        log.debug("Comparing procedure has been started.");
         try (BufferedReader br1 = new BufferedReader(new FileReader(fName1)); BufferedReader br2 = new BufferedReader(new FileReader(fName2))) {
-            files[0]="";
-            files[1]="";
+            files.add("");
+            files.add("");
             while (((line1 = br1.readLine())!= null&&((line2 = br2.readLine())!= null))) {
                 if (!line1.equals(line2))
                 {
                     System.out.println(" Modded line "+i+": "+line2);
                 }
-                files[0]+=line1;
-                files[1]+=line2;
+                files.set(0, files.get(0)+line1);
+                files.set(1, files.get(1)+line2);
                 i++;
             }
             while ((line1 = br1.readLine())!=null)
-                files[0]+=line1;
+                files.set(0, files.get(0)+line1);
             while ((line2 = br2.readLine())!=null)
-                files[1]+=line2;
+                files.set(1, files.get(1)+line2);
+            log.debug("Success.");
         }
         catch (IOException ex) {
+            log.error("Encountered problem of reading from file.\nShutdown program...");
             throw new IOException(ex);
         }
+        log.debug("Comparing procedure has been ended.");
         return files;
     }
 }
--- Level 4/Gradle/src/test/java/compare/FileCompareTest.java
+++ Level 4/Gradle/src/test/java/compare/FileCompareTest.java
@@ -4,25 +4,19 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
-
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.LogManager;
+import java.util.ArrayList;
 
 public class FileCompareTest {
-    static Logger log = LogManager.getLogger("FileCompareTest");
     @Test
-    public void compare() {
-        String fName1 = "Texts/Sample.txt";
-        String fName2 = "Texts/Modify.txt";
+    public void compare() throws IOException, IndexOutOfBoundsException {
+        String[] args = { "Texts/Sample.txt", "Texts/Modify.txt" };
         FileCompare fc = new FileCompare();
-        try {
-            String[] fs = fc.compare(fName1, fName2);
-            Assert.assertNotNull(fs[0]);
-            Assert.assertNotNull(fs[1]);
-            Assert.assertNotEquals(fs[0], fs[1]);
-        }
-        catch (IOException ex) {
-            log.error("Files don't exist or\nnames aren't correctly.");
-        }
+        Assert.assertNotNull(args[0]);
+        Assert.assertNotNull(args[1]);
+        Assert.assertNotEquals(args[0], args[1]);
+        ArrayList<String> fs = fc.compare(args[0], args[1]);
+        Assert.assertNotNull(fs.get(0));
+        Assert.assertNotNull(fs.get(1));
+        Assert.assertNotEquals(fs.get(0), fs.get(1));
     }
 }
